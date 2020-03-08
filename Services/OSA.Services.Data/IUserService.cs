568cefd7b5e5df07ef161c581b75fef30d987802
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-08 20:11:18 +0200

    Added IuserService and UserService

---
+++ Services/OSA.Services.Data/IUserService.cs
@@ -0,0 +1,15 @@
+﻿namespace OSA.Services.Data
+{
+    using System.Threading.Tasks;
+
+    using OSA.Data.Models;
+
+    public interface IUserService
+    {
+        string GetCurrentUserId();
+
+        Task<ApplicationUser> GetCurrentUserAsync();
+
+        //Task<ApplicationUser> GetUserByIdAsync(string userId);
+    }
+}
---
+++ Services/OSA.Services.Data/UserService.cs
@@ -0,0 +1,52 @@
+﻿namespace OSA.Services.Data
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using OSA.Data.Models;
+
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+        private readonly UserManager<ApplicationUser> userManager;
+        //private readonly ApplicationDbContext context;
+
+        public UserService(IHttpContextAccessor contextAccessor, UserManager<ApplicationUser> userManager)
+        {
+            this.contextAccessor = contextAccessor;
+            this.userManager = userManager;
+            //this.context = context;
+        }
+
+        public string GetCurrentUserId()
+        {
+            var currentUserId = this.contextAccessor
+                .HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier)
+                .Value;
+
+            return currentUserId;
+        }
+
+        public async Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            var currentUser = await this.userManager.GetUserAsync(this.contextAccessor.HttpContext.User);
+
+            return currentUser;
+        }
+
+        //public async Task<ApplicationUser> GetUserByIdAsync(string userId)
+        //{
+        //    if (!await this.context..AnyAsync(x => x.Id == userId))
+        //    {
+        //        throw new ArgumentException(GlobalConstants.InvalidUserIdErrorMessage);
+        //    }
+
+        //    var user = await this.userManager.FindByIdAsync(userId);
+
+        //    return user;
+        //}
+    }
+}
