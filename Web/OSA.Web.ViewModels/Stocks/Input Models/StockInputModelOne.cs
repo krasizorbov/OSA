2f3f2c7df65eec3a4ef53150d19102ff03c211a7
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-12 17:38:55 +0200

    Added Stock Input Models

---
+++ Web/OSA.Web.ViewModels/Stocks/Input Models/StockInputModelOne.cs
@@ -0,0 +1,17 @@
+﻿namespace OSA.Web.ViewModels.Stocks.Input_Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
+    public class StockInputModelOne
+    {
+        [BindRequired]
+        [Display(Name = "Company")]
+        public int CompanyId { get; set; }
+
+        public IEnumerable<SelectListItem> CompanyNames { get; set; }
+    }
+}
---
+++ Web/OSA.Web.ViewModels/Stocks/Input Models/StockInputModelTwo.cs
@@ -0,0 +1,37 @@
+﻿namespace OSA.Web.ViewModels.Stocks.Input_Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using OSA.Common;
+
+    public class StockInputModelTwo
+    {
+        private const string DateTimeFormat = @"[0-9]{2}\/[0-9]{2}\/[0-9]{4}";
+
+        [Required]
+        [MaxLength(GlobalConstants.StockNameMaxLength)]
+        public string Name { get; set; }
+
+        [Range(GlobalConstants.DecimalMinValue, GlobalConstants.DecimalMaxValue)]
+        public decimal Quantity { get; set; }
+
+        [Range(GlobalConstants.DecimalMinValue, GlobalConstants.DecimalMaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Display(Name = "Invoice Issue Date - (dd/mm/yyyy)")]
+        [RegularExpression(DateTimeFormat, ErrorMessage = GlobalConstants.InvalidDateTimeFormat)]
+        public DateTime Date { get; set; }
+
+        [BindRequired]
+        [Display(Name = "Supplier")]
+        public int SupplierId { get; set; }
+
+        public IEnumerable<SelectListItem> SupplierNames { get; set; }
+    }
+}
