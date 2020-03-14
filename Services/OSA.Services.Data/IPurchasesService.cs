e8c4f9d7c3457f509730c03d044710459be0b091
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-14 17:59:30 +0200

    Added methods to IPurchase Interface

--- Services/OSA.Services.Data/IPurchasesService.cs
+++ Services/OSA.Services.Data/IPurchasesService.cs
@@ -1,9 +1,26 @@
 ﻿namespace OSA.Services.Data
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public interface IPurchasesService
     {
-        Task AddAsync(string stockName, decimal totalQuantity, decimal totalPrice, string date, int companyId);
+        Task AddAsync(string stockName, string date, int companyId);
+
+        Task<IEnumerable<string>> GetStockNamesForCurrentMonthByCompanyIdAsync(int companyId);
+
+        Task<IEnumerable<string>> GetStockNamesForPrevoiusMonthByCompanyIdAsync(int companyId);
+
+        Task<decimal> QuantitySold(int companyId);
+
+        Task<decimal> QuantityPurchased(int companyId);
+
+        decimal TotalQuantity(int companyId);
+
+        decimal TotalPrice(int companyId);
+
+        void GetStockName(string name);
+
+        void GetDates(string startDate, string endDate, int companyId);
     }
 }
