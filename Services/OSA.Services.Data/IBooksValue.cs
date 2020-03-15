3c5475503aa4075623d24b272f3b62995506305e
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-15 12:01:45 +0200

    Added BookValue Interface

---
+++ Services/OSA.Services.Data/IBooksValue.cs
@@ -0,0 +1,9 @@
+﻿namespace OSA.Services.Data
+{
+    using System.Threading.Tasks;
+
+    public interface IBooksValue
+    {
+        Task AddAsync(decimal price, string stockName, string date, int companyId);
+    }
+}
