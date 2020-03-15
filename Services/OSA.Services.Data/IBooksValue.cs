cdf06ee95877d2bb0c24f98150200e13096dd244
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-15 12:08:58 +0200

    Added Methods to IBookValue Interface

--- Services/OSA.Services.Data/IBooksValue.cs
+++ Services/OSA.Services.Data/IBooksValue.cs
@@ -2,8 +2,14 @@
 {
     using System.Threading.Tasks;
 
+    using OSA.Data.Models;
+
     public interface IBooksValue
     {
         Task AddAsync(decimal price, string stockName, string date, int companyId);
+
+        Task<Sell> GetMonthlySells(string startDate, string endDate, int companyId);
+
+        Task<decimal> GetStockMonthlyAveragePrice(string stockName, string startDate, string endDate, int companyId);
     }
 }
