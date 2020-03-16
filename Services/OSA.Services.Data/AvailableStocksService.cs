ecefe6874e0537443831daa77f039f3914b45a2b
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-16 21:57:43 +0200

    Finished Methods Available Stock Service

--- Services/OSA.Services.Data/AvailableStocksService.cs
+++ Services/OSA.Services.Data/AvailableStocksService.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
@@ -17,6 +16,8 @@
         private const string DateFormat = "dd/MM/yyyy";
         private readonly IDeletableEntityRepository<AvailableStock> availableStockRepository;
         private readonly ApplicationDbContext context;
+        private IEnumerable<string> purchasedStockNamesForCurrentMonth;
+        private IEnumerable<string> soldStockNamesForCurrentMonth;
 
         public AvailableStocksService(IDeletableEntityRepository<AvailableStock> availableStockRepository, ApplicationDbContext context)
         {
@@ -28,6 +29,67 @@
         {
             var start_Date = DateTime.ParseExact(startDate, DateFormat, CultureInfo.InvariantCulture);
             var end_Date = DateTime.ParseExact(endDate, DateFormat, CultureInfo.InvariantCulture);
+
+            this.purchasedStockNamesForCurrentMonth = await this.GetPurchasedStockNamesByCompanyIdAsync(start_Date, end_Date, companyId);
+            this.soldStockNamesForCurrentMonth = await this.GetSoldStockNamesByCompanyIdAsync(start_Date, end_Date, companyId);
+
+            List<string> purchasedStockNames = this.purchasedStockNamesForCurrentMonth.ToList();
+            List<string> soldStockNames = this.soldStockNamesForCurrentMonth.ToList();
+
+            purchasedStockNames.AddRange(soldStockNames);
+            var stockNamesList = purchasedStockNames.Distinct();
+
+            foreach (var name in stockNamesList.OrderBy(x => x))
+            {
+                var currentPurchasedStock = await this.GetCurrentPurchasedStockNameAsync(start_Date, end_Date, name, companyId);
+                var currentSoldStock = await this.GetCurrentSoldStockNameAsync(start_Date, end_Date, name, companyId);
+                var currentBookValue = await this.GetCurrentBookValueForStockNameAsync(start_Date, end_Date, name, companyId);
+
+                if (purchasedStockNames.Contains(name) && soldStockNames.Contains(name)) // Best scenario equal material amount on both sides
+                {
+                    var availableStock = new AvailableStock
+                    {
+                        StockName = name,
+                        TotalPurchasedAmount = currentPurchasedStock.TotalQuantity,
+                        TotalPurchasedPrice = currentPurchasedStock.TotalPrice,
+                        TotalSoldAmount = currentSoldStock.TotalQuantity,
+                        BookValue = currentBookValue,
+                        Date = DateTime.ParseExact(date, DateFormat, CultureInfo.InvariantCulture),
+                        CompanyId = companyId,
+                    };
+                    await this.availableStockRepository.AddAsync(availableStock);
+                }
+                else if (!purchasedStockNames.Contains(name) && soldStockNames.Contains(name)) // No satch material in Purchases
+                {
+                    var availableStock = new AvailableStock
+                    {
+                        StockName = name,
+                        TotalPurchasedAmount = 0,
+                        TotalPurchasedPrice = 0,
+                        TotalSoldAmount = currentSoldStock.TotalQuantity,
+                        BookValue = currentBookValue,
+                        Date = DateTime.ParseExact(date, DateFormat, CultureInfo.InvariantCulture),
+                        CompanyId = companyId,
+                    };
+                    await this.availableStockRepository.AddAsync(availableStock);
+                }
+                else if (purchasedStockNames.Contains(name) && !soldStockNames.Contains(name)) // No satch material in Sells
+                {
+                    var availableStock = new AvailableStock
+                    {
+                        StockName = name,
+                        TotalPurchasedAmount = currentPurchasedStock.TotalQuantity,
+                        TotalPurchasedPrice = currentPurchasedStock.TotalPrice,
+                        TotalSoldAmount = 0,
+                        BookValue = 0,
+                        Date = DateTime.ParseExact(date, DateFormat, CultureInfo.InvariantCulture),
+                        CompanyId = companyId,
+                    };
+                    await this.availableStockRepository.AddAsync(availableStock);
+                }
+
+                await this.availableStockRepository.SaveChangesAsync();
+            }
         }
 
         public async Task<decimal> GetCurrentBookValueForStockNameAsync(DateTime startDate, DateTime endDate, string stockName, int id)
