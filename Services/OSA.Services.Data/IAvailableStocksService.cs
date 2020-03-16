3fc56d06b4f33dd85056ee282a9964153ec7648a
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-16 21:04:10 +0200

    Added Available Stock Interface

---
+++ Services/OSA.Services.Data/IAvailableStocksService.cs
@@ -0,0 +1,24 @@
+﻿namespace OSA.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using OSA.Data.Models;
+
+    public interface IAvailableStocksService
+    {
+        Task AddAsync(string startDate, string endDate, string date, int companyId);
+
+        Task<IEnumerable<string>> GetPurchasedStockNamesByCompanyIdAsync(DateTime startDate, DateTime endDate, int companyId);
+
+        Task<IEnumerable<string>> GetSoldStockNamesByCompanyIdAsync(DateTime startDate, DateTime endDate, int companyId);
+
+        Task<Purchase> GetCurrentPurchasedStockNameAsync(DateTime startDate, DateTime endDate, string stockName, int companyId);
+
+        Task<Sell> GetCurrentSoldStockNameAsync(DateTime startDate, DateTime endDate, string stockName, int companyId);
+
+        Task<BookValue> GetCurrentBookValueForStockNameAsync(DateTime startDate, DateTime endDate, string stockName, int companyId);
+    }
+}
