df1f1f3b57eb8ca8a18845b9a6637b44b2b803d2
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-17 16:52:31 +0200

    Added Expense Book View Model

---
+++ Web/OSA.Web.ViewModels/ExpenseBooks/Input Models/CreateExpenseBookInputModel.cs
@@ -0,0 +1,35 @@
+﻿namespace OSA.Web.ViewModels.ExpenseBooks.Input_Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using OSA.Common;
+
+    public class CreateExpenseBookInputModel
+    {
+        private const string DateTimeFormat = @"[0-9]{2}\/[0-9]{2}\/[0-9]{4}";
+
+        [BindRequired]
+        [Display(Name = "Company")]
+        public int CompanyId { get; set; }
+
+        public IEnumerable<SelectListItem> CompanyNames { get; set; }
+
+        [Required]
+        [Display(Name = "Start date of the month - (dd/mm/yyyy)")]
+        [RegularExpression(DateTimeFormat, ErrorMessage = GlobalConstants.InvalidDateTimeFormat)]
+        public string StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End date of the month - (dd/mm/yyyy)")]
+        [RegularExpression(DateTimeFormat, ErrorMessage = GlobalConstants.InvalidDateTimeFormat)]
+        public string EndDate { get; set; }
+
+        [Required]
+        [Display(Name = "End date of the month - (dd/mm/yyyy)")]
+        [RegularExpression(DateTimeFormat, ErrorMessage = GlobalConstants.InvalidDateTimeFormat)]
+        public string Date { get; set; }
+    }
+}
