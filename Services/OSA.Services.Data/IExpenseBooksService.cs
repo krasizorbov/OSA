e766cb68112ac958540184bc62f96409b5cfcdf0
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-17 16:23:08 +0200

    Added Expense Book Interface

---
+++ Services/OSA.Services.Data/IExpenseBooksService.cs
@@ -0,0 +1,21 @@
+﻿namespace OSA.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using OSA.Data.Models;
+
+    public interface IExpenseBooksService
+    {
+        Task AddAsync(string startDate, string endDate, string date, int companyId);
+
+        Task<List<ProductionInvoice>> GetAllProductionInvoicesByMonthAsync(DateTime startDate, DateTime endDate, int companyId);
+
+        Task<List<Receipt>> GetAllReceiptsByMonthAsync(DateTime startDate, DateTime endDate, int companyId);
+
+        Task<List<BookValue>> GetAllBookValuesByMonthAsync(DateTime startDate, DateTime endDate, int companyId);
+
+        Task<List<Sell>> GetAllSellsByMonthAsync(DateTime startDate, DateTime endDate, int companyId);
+    }
+}
