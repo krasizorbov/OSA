c497255aaafd66d829c6b3b5aa1fcfeae5ef69ed
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-17 17:58:59 +0200

    Added Methods to Service

--- Services/OSA.Services.Data/CashBooksService.cs
+++ Services/OSA.Services.Data/CashBooksService.cs
@@ -1,27 +1,48 @@
 ﻿namespace OSA.Services.Data
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using System.Linq;
     using System.Threading.Tasks;
 
+    using Microsoft.EntityFrameworkCore;
+
+    using OSA.Data;
+    using OSA.Data.Common.Repositories;
     using OSA.Data.Models;
 
     public class CashBooksService : ICashBooksService
     {
+        private readonly IDeletableEntityRepository<CashBook> cashBooksRepository;
+        private readonly ApplicationDbContext context;
+
+        public CashBooksService(IDeletableEntityRepository<CashBook> cashBooksRepository, ApplicationDbContext context)
+        {
+            this.cashBooksRepository = cashBooksRepository;
+            this.context = context;
+        }
+
         public Task AddAsync(string startDate, string endDate, string date, int companyId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ExpenseBook> GetMonthlyExpenseBook(DateTime startDate, DateTime endDate, int companyId)
+        public async Task<ExpenseBook> GetMonthlyExpenseBook(DateTime startDate, DateTime endDate, int id)
         {
-            throw new NotImplementedException();
+            var expenseBook = await this.context.ExpenseBooks
+                .Where(x => x.Date >= startDate && x.Date <= endDate && x.CompanyId == id)
+                .Select(x => x)
+                .FirstOrDefaultAsync();
+
+            return expenseBook;
         }
 
-        public Task<decimal> TotalSumStockCostAsync(DateTime startDate, DateTime endDate, int companyId)
+        public async Task<decimal> TotalSumStockCostAsync(DateTime startDate, DateTime endDate, int id)
         {
-            throw new NotImplementedException();
+            var stockCost = await this.context.Stocks
+                .Where(x => x.Date >= startDate && x.Date <= endDate && x.CompanyId == id)
+                .Sum(x => x.Price);
+
+            return stockCost;
         }
     }
 }
