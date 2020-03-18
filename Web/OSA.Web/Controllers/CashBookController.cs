e04c8e7ec4d3046a5b257c217fc17b7aeedbbb60
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-18 11:49:27 +0200

    Added Cash Book Controller

---
+++ Web/OSA.Web/Controllers/CashBookController.cs
@@ -0,0 +1,52 @@
+﻿namespace OSA.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using OSA.Services.Data;
+    using OSA.Web.ViewModels.CashBooks.Input_Models;
+
+    public class CashBookController : Controller
+    {
+        private readonly ICashBooksService cashBooksService;
+        private readonly ICompaniesService companiesService;
+
+        public CashBookController(ICashBooksService cashBooksService, ICompaniesService companiesService)
+        {
+            this.cashBooksService = cashBooksService;
+            this.companiesService = companiesService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Add()
+        {
+            var companyNames = await this.companiesService.GetAllCompaniesByUserIdAsync();
+
+            var model = new CreateCashBookInputModel
+            {
+                CompanyNames = companyNames,
+            };
+            return this.View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Add(CreateCashBookInputModel cashBookInputModel, string startDate, string endDate, int id)
+        {
+            var companyId = cashBookInputModel.CompanyId;
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            await this.cashBooksService.AddAsync(
+                cashBookInputModel.StartDate,
+                cashBookInputModel.EndDate,
+                cashBookInputModel.Date,
+                companyId);
+            return this.Redirect("/");
+        }
+    }
+}
