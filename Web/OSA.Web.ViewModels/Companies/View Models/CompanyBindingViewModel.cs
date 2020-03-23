5ee8f0282577ab3c7b7c5fc8ddeba585ba7a9244
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-23 16:30:33 +0200

    Added Company Binding View Model

---
+++ Web/OSA.Web.ViewModels/Companies/View Models/CompanyBindingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.Companies.View_Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CompanyBindingViewModel
+    {
+        public string Name { get; set; }
+
+        public string Bulstat { get; set; }
+    }
+}
