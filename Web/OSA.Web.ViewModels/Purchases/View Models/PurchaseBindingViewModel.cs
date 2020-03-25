9e8deb3490ee6682a8766aa3418749b1fa668836
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-25 17:19:28 +0200

    Added Purchase binding view model

---
+++ Web/OSA.Web.ViewModels/Purchases/View Models/PurchaseBindingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.Purchases.View_Models
+{
+    using System.Collections.Generic;
+
+    using OSA.Data.Models;
+
+    public class PurchaseBindingViewModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<Purchase> Purchases { get; set; }
+    }
+}
