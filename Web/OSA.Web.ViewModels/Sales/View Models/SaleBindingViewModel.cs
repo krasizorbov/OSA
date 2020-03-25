070f2972bc952d825be9b33162388b05b8e77fee
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-25 14:21:12 +0200

    Added Sale Binding View Model

---
+++ Web/OSA.Web.ViewModels/Sales/View Models/SaleBindingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.Sales.View_Models
+{
+    using System.Collections.Generic;
+
+    using OSA.Data.Models;
+
+    public class SaleBindingViewModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<Sale> Sales { get; set; }
+    }
+}
