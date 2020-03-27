654761ead246cd2f9bbf95ab3a66fb250488be95
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-27 15:47:53 +0200

    Added Receipts Binding View Model

---
+++ Web/OSA.Web.ViewModels/Receipts/View Models/ReceiptBindingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.Receipts.View_Models
+{
+    using System.Collections.Generic;
+
+    using OSA.Data.Models;
+
+    public class ReceiptBindingViewModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<Receipt> Receipts { get; set; }
+    }
+}
