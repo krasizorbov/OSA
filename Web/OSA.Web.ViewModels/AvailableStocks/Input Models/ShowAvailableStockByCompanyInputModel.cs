296ceb408082a8ba06c7ceaf50c68da9f3d74c4d
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-27 09:57:31 +0200

    Added Available Stock by Company Input Model

---
+++ Web/OSA.Web.ViewModels/AvailableStocks/Input Models/ShowAvailableStockByCompanyInputModel.cs
@@ -0,0 +1,28 @@
+﻿namespace OSA.Web.ViewModels.AvailableStocks.Input_Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using OSA.Common;
+
+    public class ShowAvailableStockByCompanyInputModel
+    {
+        [BindRequired]
+        [Display(Name = "Company")]
+        public int CompanyId { get; set; }
+
+        public IEnumerable<SelectListItem> CompanyNames { get; set; }
+
+        [Required]
+        [Display(Name = "Start Date - (dd/mm/yyyy)")]
+        [RegularExpression(GlobalConstants.DateTimeRegexFormat, ErrorMessage = GlobalConstants.InvalidDateTimeFormat)]
+        public string StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date - (dd/mm/yyyy)")]
+        [RegularExpression(GlobalConstants.DateTimeRegexFormat, ErrorMessage = GlobalConstants.InvalidDateTimeFormat)]
+        public string EndDate { get; set; }
+    }
+}
