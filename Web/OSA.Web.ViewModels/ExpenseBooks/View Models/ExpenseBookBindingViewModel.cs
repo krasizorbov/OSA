0759b8c5b3e898c797c02dfda3387f56831a2c4e
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-27 16:12:23 +0200

    Added Expense Book Binding View Model

---
+++ Web/OSA.Web.ViewModels/ExpenseBooks/View Models/ExpenseBookBindingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.ExpenseBooks.View_Models
+{
+    using System.Collections.Generic;
+
+    using OSA.Data.Models;
+
+    public class ExpenseBookBindingViewModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<ExpenseBook> ExpenseBooks { get; set; }
+    }
+}
