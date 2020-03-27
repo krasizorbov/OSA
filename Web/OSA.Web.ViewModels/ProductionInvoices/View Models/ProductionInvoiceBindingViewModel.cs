68e82bec127ddf999c46539188ddef3e33e2bfbe
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-27 11:18:28 +0200

    Added Production Invoice Binding View Model

---
+++ Web/OSA.Web.ViewModels/ProductionInvoices/View Models/ProductionInvoiceBindingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.ProductionInvoices.View_Models
+{
+    using System.Collections.Generic;
+
+    using OSA.Data.Models;
+
+    public class ProductionInvoiceBindingViewModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<ProductionInvoice> ProductionInvoices { get; set; }
+    }
+}
