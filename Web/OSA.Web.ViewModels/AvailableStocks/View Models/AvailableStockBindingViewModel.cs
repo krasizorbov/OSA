63a3e40897fb4925f80f4a4a9fc4010129d99e6c
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-27 09:54:21 +0200

    Added Available Stock Binding View Model

---
+++ Web/OSA.Web.ViewModels/AvailableStocks/View Models/AvailableStockBindingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.AvailableStocks.View_Models
+{
+    using System.Collections.Generic;
+
+    using OSA.Data.Models;
+
+    public class AvailableStockBindingViewModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<AvailableStock> AvailableStocks { get; set; }
+    }
+}
