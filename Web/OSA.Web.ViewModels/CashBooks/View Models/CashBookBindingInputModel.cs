c8e93f8825606989aabf92249d9f4d17855eee59
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-28 18:21:59 +0200

    Added Cash Book Binding View Model

---
+++ Web/OSA.Web.ViewModels/CashBooks/View Models/CashBookBindingInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OSA.Web.ViewModels.CashBooks.View_Models
+{
+    using System.Collections.Generic;
+
+    using OSA.Data.Models;
+
+    public class CashBookBindingInputModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<CashBook> CashBooks { get; set; }
+    }
+}
