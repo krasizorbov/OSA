571bdffcc1e007a971a41a54f9920e9ab4427cc5
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-28 18:41:52 +0200

    Added Methods to Cash Book Controller

--- Web/OSA.Web/Controllers/CashBookController.cs
+++ Web/OSA.Web/Controllers/CashBookController.cs
@@ -10,10 +10,10 @@
     using OSA.Services.Data;
     using OSA.Web.ValidationEnum;
     using OSA.Web.ViewModels.CashBooks.Input_Models;
+    using OSA.Web.ViewModels.CashBooks.View_Models;
 
     public class CashBookController : BaseController
     {
-        private const string DateFormat = "dd/MM/yyyy";
         private const string CashBookErrorMessage = "Cash book for the month already done!";
 
         private readonly ICashBooksService cashBooksService;
@@ -46,8 +46,8 @@
         [HttpPost]
         public async Task<IActionResult> Add(CreateCashBookInputModel cashBookInputModel, string startDate, string endDate, int id)
         {
-            var start_Date = DateTime.ParseExact(startDate, DateFormat, CultureInfo.InvariantCulture);
-            var end_Date = DateTime.ParseExact(endDate, DateFormat, CultureInfo.InvariantCulture);
+            var start_Date = DateTime.ParseExact(startDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var end_Date = DateTime.ParseExact(endDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
 
             var companyId = cashBookInputModel.CompanyId;
             var cashBook = await this.cashBooksService.CashBookExistAsync(start_Date, end_Date, companyId);
@@ -76,6 +76,59 @@
                 companyId);
             this.TempData["message"] = GlobalConstants.SuccessfullyRegistered;
             return this.Redirect("/");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> GetCompany()
+        {
+            var companyNames = await this.companiesService.GetAllCompaniesByUserIdAsync();
+
+            if (companyNames.Count == 0)
+            {
+                this.SetFlash(FlashMessageType.Error, GlobalConstants.CompanyErrorMessage);
+            }
+
+            var model = new ShowCashBookByCompanyInputModel
+            {
+                CompanyNames = companyNames,
+            };
+            return this.View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> GetCompany(ShowCashBookByCompanyInputModel inputModel)
+        {
+            var companyName = await this.companiesService.GetCompanyNameByIdAsync(inputModel.CompanyId);
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            return this.RedirectToAction("GetCashBook", "CashBook", new
+            {
+                id = inputModel.CompanyId,
+                name = companyName,
+                startDate = inputModel.StartDate,
+                endDate = inputModel.EndDate,
+            });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> GetCashBook(int id, string name, string startDate, string endDate)
+        {
+            var start_Date = DateTime.ParseExact(startDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var end_Date = DateTime.ParseExact(endDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+
+            var cashBooks = await this.cashBooksService.GetCashBooksByCompanyIdAsync(start_Date, end_Date, id);
+
+            var model = new CashBookBindingViewModel
+            {
+                Name = name,
+                CashBooks = cashBooks,
+            };
+
+            return this.View(model);
         }
     }
 }
