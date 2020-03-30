e154c87f713e6da89d9562c40f6c64116f3a1ea5
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-03-30 11:54:05 +0300

    Refactoring

--- Web/OSA.Web/Controllers/ExpenseBookController.cs
+++ Web/OSA.Web/Controllers/ExpenseBookController.cs
@@ -8,6 +8,7 @@
     using Microsoft.AspNetCore.Mvc;
     using OSA.Common;
     using OSA.Services.Data;
+    using OSA.Services.Data.Interfaces;
     using OSA.Web.ValidationEnum;
     using OSA.Web.ViewModels.ExpenseBooks.Input_Models;
     using OSA.Web.ViewModels.ExpenseBooks.View_Models;
@@ -18,11 +19,13 @@
 
         private readonly IExpenseBooksService expenseBooksService;
         private readonly ICompaniesService companiesService;
+        private readonly IDateTimeValidationService dateTimeValidationService;
 
-        public ExpenseBookController(IExpenseBooksService expenseBooksService, ICompaniesService companiesService)
+        public ExpenseBookController(IExpenseBooksService expenseBooksService, ICompaniesService companiesService, IDateTimeValidationService dateTimeValidationService)
         {
             this.expenseBooksService = expenseBooksService;
             this.companiesService = companiesService;
+            this.dateTimeValidationService = dateTimeValidationService;
         }
 
         [Authorize]
@@ -39,19 +42,35 @@
 
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult> Add(CreateExpenseBookInputModel expenseBookInputModel, string startDate, string endDate, int id)
+        public async Task<IActionResult> Add(CreateExpenseBookInputModel expenseBookInputModel, string startDate, string endDate)
         {
-            var start_Date = DateTime.ParseExact(startDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
-            var end_Date = DateTime.ParseExact(endDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var isValidStartDate = this.dateTimeValidationService.IsValidDateTime(startDate);
+            var isValidEndDate = this.dateTimeValidationService.IsValidDateTime(endDate);
 
-            var companyId = expenseBookInputModel.CompanyId;
-            var expenseBook = await this.expenseBooksService.ExpenseBookExistAsync(start_Date, end_Date, companyId);
-
-            if (!this.ModelState.IsValid)
+            if (!this.ModelState.IsValid || !isValidStartDate || !isValidEndDate)
             {
-                return this.View();
+                var companyNames = await this.companiesService.GetAllCompaniesByUserIdAsync();
+
+                if (!isValidStartDate)
+                {
+                    this.ModelState.AddModelError(nameof(expenseBookInputModel.StartDate), expenseBookInputModel.StartDate + GlobalConstants.InvalidDateTime);
+                }
+
+                if (!isValidEndDate)
+                {
+                    this.ModelState.AddModelError(nameof(expenseBookInputModel.EndDate), expenseBookInputModel.EndDate + GlobalConstants.InvalidDateTime);
+                }
+
+                var model = new CreateExpenseBookInputModel
+                {
+                    CompanyNames = companyNames,
+                };
+                return this.View(model);
             }
 
+            var start_Date = DateTime.ParseExact(startDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var end_Date = DateTime.ParseExact(endDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var expenseBook = await this.expenseBooksService.ExpenseBookExistAsync(start_Date, end_Date, expenseBookInputModel.CompanyId);
             if (expenseBook != null)
             {
                 var companyNames = await this.companiesService.GetAllCompaniesByUserIdAsync();
@@ -68,7 +87,7 @@
             await this.expenseBooksService.AddAsync(
                 expenseBookInputModel.StartDate,
                 expenseBookInputModel.EndDate,
-                companyId);
+                expenseBookInputModel.CompanyId);
             this.TempData["message"] = GlobalConstants.SuccessfullyRegistered;
             return this.Redirect("/");
         }
@@ -92,12 +111,31 @@
 
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult> GetCompany(ShowExpenseBookByCompanyInputModel inputModel)
+        public async Task<IActionResult> GetCompany(ShowExpenseBookByCompanyInputModel inputModel, string startDate, string endDate)
         {
             var companyName = await this.companiesService.GetCompanyNameByIdAsync(inputModel.CompanyId);
-            if (!this.ModelState.IsValid)
+            var isValidStartDate = this.dateTimeValidationService.IsValidDateTime(startDate);
+            var isValidEndDate = this.dateTimeValidationService.IsValidDateTime(endDate);
+
+            if (!this.ModelState.IsValid || !isValidStartDate || !isValidEndDate)
             {
-                return this.View();
+                var companyNames = await this.companiesService.GetAllCompaniesByUserIdAsync();
+
+                if (!isValidStartDate)
+                {
+                    this.ModelState.AddModelError(nameof(inputModel.StartDate), inputModel.StartDate + GlobalConstants.InvalidDateTime);
+                }
+
+                if (!isValidEndDate)
+                {
+                    this.ModelState.AddModelError(nameof(inputModel.EndDate), inputModel.EndDate + GlobalConstants.InvalidDateTime);
+                }
+
+                var model = new ShowExpenseBookByCompanyInputModel
+                {
+                    CompanyNames = companyNames,
+                };
+                return this.View(model);
             }
 
             return this.RedirectToAction("GetExpenseBook", "ExpenseBook", new
