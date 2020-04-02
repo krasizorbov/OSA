920495c9940104f6d0f1c17bbeb88708e1d6444c
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-04-02 22:14:08 +0300

    Added Migration Change Average Price to string Available Stock

---
+++ Data/OSA.Data/Migrations/20200402191305_ChangeAveragePriceToStringAvailableStock.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OSA.Data.Migrations
+{
+    public partial class ChangeAveragePriceToStringAvailableStock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AveragePrice",
+                table: "AvailableStocks",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AveragePrice",
+                table: "AvailableStocks",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
