136f78a6d9a1e33442a10e1259d6ab93b050c25a
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-04-10 18:32:18 +0300

    Added method to Purchases Tests Services

--- Tests/OSA.Services.Data.Tests/PurchasesServiceTests.cs
+++ Tests/OSA.Services.Data.Tests/PurchasesServiceTests.cs
@@ -1,10 +1,8 @@
 ﻿namespace OSA.Services.Data.Tests
 {
     using System;
-    using System.Collections.Generic;
     using System.Globalization;
     using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
 
     using OSA.Common;
@@ -16,7 +14,6 @@
         private const string StartDate = "01/01/2020";
         private const string EndDate = "31/01/2020";
         private const string StockName = "sugar";
-        private List<string> stockNamesForCurrentMonth;
         private IPurchasesService ips;
 
         [Fact]
@@ -87,7 +84,7 @@
             {
                 Id = 1,
                 CreatedOn = startDate,
-                Name = "Sugar",
+                Name = StockName,
                 Quantity = 20.00M,
                 Price = 30.00M,
                 Date = startDate,
@@ -99,7 +96,7 @@
             {
                 Id = 2,
                 CreatedOn = startDate,
-                Name = "Sugar",
+                Name = StockName,
                 Quantity = 20.00M,
                 Price = 30.00M,
                 Date = startDate,
@@ -111,7 +108,7 @@
             {
                 Id = 3,
                 CreatedOn = startDate,
-                Name = "Sugar",
+                Name = StockName,
                 Quantity = 20.00M,
                 Price = 30.00M,
                 Date = startDate,
@@ -124,6 +121,32 @@
             await context.Stocks.AddAsync(stock3);
             await context.SaveChangesAsync();
             var result = await this.ips.GetStockNamesForCurrentMonthByCompanyIdAsync(startDate, endDate, 1);
+            Assert.Equal("1", result.Count().ToString());
+        }
+
+        [Fact]
+
+        public async Task GetStockNamesForPrevoiusMonthByCompanyIdAsyncReturnsCorrectCount()
+        {
+            var context = InitializeContext.CreateContextForInMemory();
+            this.ips = new PurchasesService(context);
+            var startDate = DateTime.ParseExact(StartDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(EndDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+
+            var purchase = new Purchase
+            {
+                Id = 1,
+                CreatedOn = startDate.AddDays(-10),
+                StockName = StockName,
+                TotalQuantity = 20.00M,
+                TotalPrice = 30.00M,
+                Date = startDate.AddDays(-10),
+                CompanyId = 1,
+            };
+
+            await context.Purchases.AddAsync(purchase);
+            await context.SaveChangesAsync();
+            var result = await this.ips.GetStockNamesForPrevoiusMonthByCompanyIdAsync(startDate, endDate, 1);
             Assert.Equal("1", result.Count().ToString());
         }
     }
