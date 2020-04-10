83da07716278824ea09df81a16a3c54e5fdf8b76
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-04-10 12:47:49 +0300

    Added Purchases Service Tests

---
+++ Tests/OSA.Services.Data.Tests/PurchasesServiceTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSA.Services.Data.Tests
+{
+    public class PurchasesServiceTests
+    {
+        private IPurchasesService ips;
+    }
+}
