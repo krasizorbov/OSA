acd5f90cfe494544064e5a0c5396334466643d77
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-04-11 17:37:40 +0300

    Added Methods to Expense Book Tests

--- Tests/OSA.Services.Data.Tests/ExpenseBooksServiceTests.cs
+++ Tests/OSA.Services.Data.Tests/ExpenseBooksServiceTests.cs
@@ -1,18 +1,19 @@
-﻿using OSA.Common;
-using OSA.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
-
-namespace OSA.Services.Data.Tests
+﻿namespace OSA.Services.Data.Tests
 {
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using OSA.Common;
+    using OSA.Data.Models;
+    using Xunit;
+
     public class ExpenseBooksServiceTests
     {
         private const string StartDate = "01/01/2020";
         private const string EndDate = "31/01/2020";
+        private const string StockName = "sugar";
         private IExpenseBooksService iebs;
 
         [Fact]
@@ -46,7 +47,6 @@ namespace OSA.Services.Data.Tests
             var context = InitializeContext.CreateContextForInMemory();
             this.iebs = new ExpenseBooksService(context);
             var startDate = DateTime.ParseExact(StartDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
-            var endDate = DateTime.ParseExact(EndDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
 
             var expenseBook = new ExpenseBook
             {
@@ -64,6 +64,109 @@ namespace OSA.Services.Data.Tests
             await context.SaveChangesAsync();
             var result = await this.iebs.DeleteAsync(2);
             Assert.True(result == null);
+        }
+
+        [Fact]
+        public async Task ExpenseBookExistAsyncReturnsExpenseBook()
+        {
+            var context = InitializeContext.CreateContextForInMemory();
+            this.iebs = new ExpenseBooksService(context);
+            var startDate = DateTime.ParseExact(StartDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(EndDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+
+            var expenseBook = new ExpenseBook
+            {
+                Id = 1,
+                CreatedOn = startDate,
+                TotalExternalCost = 20.00M,
+                TotalSalaryCost = 20.00M,
+                TotalBookValue = 20.00M,
+                Profit = 100.00M,
+                Date = startDate,
+                CompanyId = 1,
+            };
+
+            await context.ExpenseBooks.AddAsync(expenseBook);
+            await context.SaveChangesAsync();
+            var result = await this.iebs.ExpenseBookExistAsync(startDate, endDate, 1);
+            Assert.Equal(expenseBook, result);
+        }
+
+        [Fact]
+        public async Task ExpenseBookExistAsyncReturnsNull()
+        {
+            var context = InitializeContext.CreateContextForInMemory();
+            this.iebs = new ExpenseBooksService(context);
+            var startDate = DateTime.ParseExact(StartDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(EndDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+
+            var expenseBook = new ExpenseBook
+            {
+                Id = 1,
+                CreatedOn = startDate,
+                TotalExternalCost = 20.00M,
+                TotalSalaryCost = 20.00M,
+                TotalBookValue = 20.00M,
+                Profit = 100.00M,
+                Date = startDate,
+                CompanyId = 1,
+            };
+
+            await context.ExpenseBooks.AddAsync(expenseBook);
+            await context.SaveChangesAsync();
+            var result = await this.iebs.ExpenseBookExistAsync(startDate, endDate, 2);
+            Assert.True(result == null);
+        }
+
+        [Fact]
+        public async Task GetAllProductionInvoicesByMonthAsyncReturnsCorrectCount()
+        {
+            var context = InitializeContext.CreateContextForInMemory();
+            this.iebs = new ExpenseBooksService(context);
+            var startDate = DateTime.ParseExact(StartDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(EndDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+
+            var productionInvoice = new ProductionInvoice
+            {
+                Id = 1,
+                CreatedOn = startDate,
+                InvoiceNumber = "1",
+                ExternalCost = 20.00M,
+                Salary = 120.00M,
+                Date = startDate,
+                CompanyId = 1,
+            };
+
+            await context.ProductionInvoices.AddAsync(productionInvoice);
+            await context.SaveChangesAsync();
+            var result = await this.iebs.GetAllProductionInvoicesByMonthAsync(startDate, endDate, 1);
+            Assert.Equal("1", result.Count().ToString());
+        }
+
+        [Fact]
+        public async Task GetAllSalesByMonthAsyncReturnsCorrectCount()
+        {
+            var context = InitializeContext.CreateContextForInMemory();
+            this.iebs = new ExpenseBooksService(context);
+            var startDate = DateTime.ParseExact(StartDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(EndDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+
+            var sale = new Sale
+            {
+                Id = 1,
+                CreatedOn = startDate,
+                StockName = StockName,
+                TotalPrice = 200.00M,
+                ProfitPercent = 120,
+                AveragePrice = "1.5",
+                Date = startDate,
+                CompanyId = 1,
+            };
+
+            await context.Sales.AddAsync(sale);
+            await context.SaveChangesAsync();
+            var result = await this.iebs.GetAllSalesByMonthAsync(startDate, endDate, 1);
+            Assert.Equal("1", result.Count().ToString());
         }
     }
 }
