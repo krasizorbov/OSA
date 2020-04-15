943627746b5803a5c38da435af70572f28eb390d
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-04-15 14:49:54 +0300

    Added Method to Stock Tests

--- Tests/OSA.Services.Data.Tests/StocksServiceTests.cs
+++ Tests/OSA.Services.Data.Tests/StocksServiceTests.cs
@@ -222,6 +222,32 @@
             var context = InitializeContext.CreateContextForInMemory();
             this.ss = new StocksService(context);
             var controller = new StockController(moqStockService.Object, moqCompanyService.Object, moqInvoiceService.Object, moqDateTimeService.Object);
+            var stockModel = new CreateStockInputModelTwo
+            {
+                Name = "sugar",
+                Price = 300.00M,
+                Quantity = 200.00M,
+                Date = StartDate,
+                Invoices = new List<SelectListItem> { new SelectListItem { Value = "1", Text = "1", } },
+            };
+            var moqStartDate = moqDateTimeService.Setup(x => x.IsValidDateTime("13/31/2020")).Returns(false);
+            var result = await controller.AddPartTwo(stockModel, 1, StartDate);
+            var view = controller.View(stockModel) as ViewResult;
+            var actual = controller.ModelState;
+            Assert.True(actual.IsValid == false);
+        }
+
+        [Fact]
+
+        public async Task GetCompanyReturnsModelStateDateTimeFormatError()
+        {
+            var moqInvoiceService = new Mock<IInvoicesService>();
+            var moqCompanyService = new Mock<ICompaniesService>();
+            var moqStockService = new Mock<IStocksService>();
+            var moqDateTimeService = new Mock<IDateTimeValidationService>();
+            var context = InitializeContext.CreateContextForInMemory();
+            this.ss = new StocksService(context);
+            var controller = new StockController(moqStockService.Object, moqCompanyService.Object, moqInvoiceService.Object, moqDateTimeService.Object);
             var stockModel = new ShowStockByCompanyInputModel
             {
                 StartDate = "13/01/2020",
@@ -277,6 +303,46 @@
             Assert.Equal("20.00", stockModel.Stocks.Select(x => x.Quantity).ElementAt(0).ToString());
             Assert.Equal("1", stockModel.Stocks.Select(x => x.InvoiceId).ElementAt(0).ToString());
             Assert.Equal("1", stockModel.Stocks.Select(x => x.CompanyId).ElementAt(0).ToString());
+        }
+
+        [Fact]
+
+        public async Task DeleteReturnsCorrectModel()
+        {
+            var startDate = DateTime.ParseExact(StartDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(EndDate, GlobalConstants.DateFormat, CultureInfo.InvariantCulture);
+            var moqInvoiceService = new Mock<IInvoicesService>();
+            var moqCompanyService = new Mock<ICompaniesService>();
+            var moqStockService = new Mock<IStocksService>();
+            var moqDateTimeService = new Mock<IDateTimeValidationService>();
+            var context = InitializeContext.CreateContextForInMemory();
+            this.ss = new StocksService(context);
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            var expected = tempData["message"] = "Data has been deleted successfully!";
+            var controller = new StockController(moqStockService.Object, moqCompanyService.Object, moqInvoiceService.Object, moqDateTimeService.Object)
+            {
+                TempData = tempData,
+            };
+
+            var stock = new Stock
+            {
+                Id = 1,
+                CreatedOn = startDate,
+                Name = "Sugar",
+                Quantity = 20.00M,
+                Price = 30.00M,
+                Date = startDate,
+                InvoiceId = 1,
+                CompanyId = 1,
+            };
+            await context.Stocks.AddAsync(stock);
+            await context.SaveChangesAsync();
+            var listId = new List<int> { 1 };
+            moqStockService.Setup(x => x.DeleteAsync(new List<int> { 1 })).Returns(Task.FromResult(new List<Stock> { stock }));
+            var result = await controller.Delete(listId);
+            var actual = controller.TempData;
+            Assert.Equal(expected, actual.Values.ElementAt(0));
         }
     }
 }
